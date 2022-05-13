a6ecb94249c056d6b14baf4dcd910e7404cd7123
Sung-won Kim <raptol@gmail.com>
2022-05-13 10:45:09 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/******************************************
+*****************************************/
+
+/*** 
+ * `quotes` array 
+***/
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * The code will look like the following. You need to complete it.
+
+***/
+
+document.getElementById('load-quote').addEventListener...
